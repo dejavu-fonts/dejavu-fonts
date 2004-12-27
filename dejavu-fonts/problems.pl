3e612047f2e327d656fa08f052f279dbcca1039d
src <src@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2004-12-27 18:08:30 +0000

    added monospace problem detection

--- dejavu-fonts/problems.pl
+++ dejavu-fonts/problems.pl
@@ -6,6 +6,11 @@
 # (c)2004 Stepan Roh
 # usage: ./problems.pl sfd_files+
 
+# detected problems:
+#   colorized glyphs with content
+#   glyphs in monospaced face with different width
+#   monospaced font (with Mono in name) without indication in Panose (and vice-versa)
+
 sub process_sfd_file($);
 
 sub process_sfd_file($) {
@@ -17,11 +22,16 @@ sub process_sfd_file($) {
   my $dec_enc = 0;
   my $colorized;
   my $flags;
+  my ($fontname, $panose, $is_mono_name, $is_mono_panose);
+  my $is_mono = 0;
+  my $font_width = -1;
+  my $curwidth = 0;
   while (<SFD>) {
     if (/^StartChar:\s*(\S+)\s*$/) {
       $curchar = $1;
       $hex_enc = '';
       $dec_enc = 0;
+      $curwidth = -1;
       undef $colorized;
       undef $flags;
     } elsif (/^Colour:\s*(\S+)\s*/) {
@@ -33,11 +43,33 @@ sub process_sfd_file($) {
       if ($2 > -1) {
         $hex_enc = sprintf ('%04x', $2);
       }
+    } elsif (/^Width:\s*(\S+)\s*/) {
+      $curwidth = $1;
     } elsif (/^EndChar\s*$/) {
       if (defined $colorized && defined $flags) {
-        print $sfd_file, ': ', $curchar, ' ', $dec_enc, ($hex_enc ? ' U+'.$hex_enc : '') , ': color=', $colorized, ', flags=', $flags, "\n";
+        print $sfd_file, ': colorized content: ', $curchar, ' ', $dec_enc, ($hex_enc ? ' U+'.$hex_enc : '') , ': color=', $colorized, ', flags=', $flags, "\n";
       }
+      if ($curwidth == -1) {
+        print $sfd_file, ': glyph w/o width: ', $curchar, ' ', $dec_enc, ($hex_enc ? ' U+'.$hex_enc : ''), "\n";
+      } elsif ($is_mono && defined $flags) {
+        if ($font_width == -1) {
+          $font_width = $curwidth;
+        } elsif ($curwidth != $font_width) {
+          print $sfd_file, ': incorrect width: ', $curchar, ' ', $dec_enc, ($hex_enc ? ' U+'.$hex_enc : ''), ': font width=', $font_width, ', glyph width=', $curwidth, "\n";
+        }
+      }
+    } elsif (/^FontName:\s*(.*?)\s*$/) {
+      $fontname = $1;
+      $is_mono_name = ($fontname =~ /mono/i);
+      $is_mono = 1 if ($is_mono_name);
+    } elsif (/^Panose:\s*(.*?)\s*$/) {
+      $panose = $1;
+      $is_mono_panose = ((split(/\s+/, $panose))[3] == 9);
+      $is_mono = 1 if ($is_mono_panose);
     }
+  }
+  if ($is_mono_name != $is_mono_panose) {
+    print $sfd_file, ': mixed monospace: font name=', $fontname, ', panose=', $panose, "\n";
   }
   close (SFD);
 }
