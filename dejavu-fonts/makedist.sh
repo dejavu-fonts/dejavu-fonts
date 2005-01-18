0d4221868421fd99bdf61c30f761ab76f2ece295
src <src@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2005-01-18 17:46:10 +0000

    pack more files

--- dejavu-fonts/makedist.sh
+++ dejavu-fonts/makedist.sh
@@ -5,8 +5,8 @@
 version=$1
 echo Making distribution of DejaVu fonts $version
 mkdir packaged/dejavu-sfd-$version
-cp *.sfd *.pe README LICENSE AUTHORS NEWS BUGS status.txt packaged/dejavu-sfd-$version
+cp *.sfd *.pe *.me *.sh *.pl README LICENSE AUTHORS NEWS BUGS *.txt packaged/dejavu-sfd-$version
 (cd packaged; tar czvf dejavu-sfd-$version.tar.gz dejavu-sfd-$version)
 mkdir packaged/dejavu-ttf-$version
-cp generated/*.ttf README LICENSE AUTHORS NEWS BUGS status.txt packaged/dejavu-ttf-$version
+cp generated/*.ttf README LICENSE AUTHORS NEWS BUGS *.txt packaged/dejavu-ttf-$version
 (cd packaged; tar czvf dejavu-ttf-$version.tar.gz dejavu-ttf-$version)
