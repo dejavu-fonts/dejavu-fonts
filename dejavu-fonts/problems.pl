5f5d28f9534c114b37a01361b611e1ae97f39ce0
src <src@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2005-03-27 23:18:13 +0000

    check for different set of mapped content glyphs

--- dejavu-fonts/problems.pl
+++ dejavu-fonts/problems.pl
@@ -13,13 +13,17 @@
 #   ligature in colorized glyph (due to bug in FF it causes problems on Mac OS X)
 #   ligature in empty glyph
 #   W: ligature referencing colorized or missing glyphs
+#   different set of mapped content glyphs (first SFD file specified on command line is taken as an etalon)
 
 sub process_sfd_file($$);
+
+# glyph name => ( 'dec_enc' => dec_enc, 'hex_enc' => hex_enc )
+%glyphs = ();
+$glyphs_loaded = 0;
 
 sub process_sfd_file($$) {
   my ($sfd_file, $with_warns) = @_;
   
-  open (SFD, $sfd_file) || die "Unable to open $sfd_file : $!\n";
   my $curchar = '';
   my $hex_enc = '';
   my $dec_enc = 0;
@@ -33,6 +37,7 @@ sub process_sfd_file($$) {
   my $is_empty = 1;
   my %content_glyphs = ();
   my @ligature_refs = ();
+  open (SFD, $sfd_file) || die "Unable to open $sfd_file : $!\n";
   while (<SFD>) {
     if (/^StartChar:\s*(\S+)\s*$/) {
       $curchar = $1;
@@ -66,6 +71,17 @@ sub process_sfd_file($$) {
         $content_glyphs{$curchar}{'dec_enc'} = $dec_enc;
         $content_glyphs{$curchar}{'hex_enc'} = $hex_enc;
         @{$content_glyphs{$curchar}{'ligature'}} = @ligature_refs;
+        # only mapped glyphs
+        if ($hex_enc) {
+          if ($glyphs_loaded) {
+            if (!exists $glyphs{$curchar}) {
+              print $sfd_file, ': etalon-free glyph: ', $curchar, ' ', $dec_enc, ($hex_enc ? ' U+'.$hex_enc : ''), "\n";
+            }
+          } else {
+            $glyphs{$curchar}{'dec_enc'} = $dec_enc;
+            $glyphs{$curchar}{'hex_enc'} = $hex_enc;
+          }
+        }
       }
       if (defined $colorized && defined $flags && ($flags =~ /W/)) {
         print $sfd_file, ': colorized content: ', $curchar, ' ', $dec_enc, ($hex_enc ? ' U+'.$hex_enc : '') , ': color=', $colorized, ', flags=', $flags, "\n";
@@ -98,6 +114,7 @@ sub process_sfd_file($$) {
       $is_mono = 1 if ($is_mono_panose);
     }
   }
+  close (SFD);
   if ($is_mono_name != $is_mono_panose) {
     print $sfd_file, ': mixed monospace: font name=', $fontname, ', panose=', $panose, "\n";
   }
@@ -110,7 +127,16 @@ sub process_sfd_file($$) {
       }
     }
   }
-  close (SFD);
+  if ($glyphs_loaded) {
+    foreach $glyph (sort { $glyphs{$a}{'dec_enc'} <=> $glyphs{$b}{'dec_enc'} } keys %glyphs) {
+      my $dec_enc = $glyphs{$glyph}{'dec_enc'};
+      my $hex_enc = $glyphs{$glyph}{'hex_enc'};
+      if (!exists $content_glyphs{$glyph}) {
+        print $sfd_file, ': missing glyph: ', $glyph, ' ', $dec_enc, ($hex_enc ? ' U+'.$hex_enc : ''), "\n";
+      }
+    }
+  }
+  $glyphs_loaded = 1;
 }
 
 if (!@ARGV) {
