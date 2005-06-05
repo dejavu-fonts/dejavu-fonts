59ebcf37f27a91c024d13f9066687c79b7899695
src <src@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2005-06-05 17:52:24 +0000

    compute unicode coverage for each font (sub)family

--- dejavu-fonts/unicover.pl
+++ dejavu-fonts/unicover.pl
@@ -3,15 +3,15 @@
 # $Id$
 
 # unicode coverage analyzator
-# (c)2004 Stepan Roh
-# usage: ./unicover.pl unicode_data_file blocks_file sfd_file
+# (c)2004,2005 Stepan Roh
+# usage: ./unicover.pl unicode_data_file blocks_file sfd_file1 label1 (sfd_file2 label2...)
 #  unicode data file can be downloaded from http://www.unicode.org/Public/UNIDATA/UnicodeData.txt
 #  blocks file can be downloaded from http://www.unicode.org/Public/UNIDATA/Blocks.txt
 
 sub parse_blocks_file($);
 sub parse_unicode_data_file($);
 sub parse_sfd_file($);
-sub inc_coverage($);
+sub inc_coverage($$);
 sub print_coverage();
 sub disable_char($);
 
@@ -25,7 +25,7 @@ if ($debug) {
   $Data::Dumper::Purity = 1;
 }
 
-# map (start dec => ( 'name' => block name, 'end' => end dec, 'coverage' => coverage, 'disabled' => ( disabled chars map* ) )
+# map (start dec => ( 'name' => block name, 'end' => end dec, 'coverage' => ( sfd_file => coverage ), 'disabled' => ( disabled chars map* ) )
 %blocks = ();
 
 sub parse_blocks_file($) {
@@ -68,14 +68,14 @@ sub parse_unicode_data_file($) {
   close (F);
 }
 
-sub inc_coverage($) {
-  my ($dec_enc) = @_;
+sub inc_coverage($$) {
+  my ($sfd_file, $dec_enc) = @_;
   
   foreach $block_start (keys %blocks) {
     my ($block_end) = $blocks{$block_start}{'end'};
     if (($dec_enc >= $block_start) && ($dec_enc <= $block_end)) {
       if (!exists $blocks{$block_start}{'disabled'}{$dec_enc}) {
-        $blocks{$block_start}{'coverage'}++;
+        $blocks{$block_start}{'coverage'}{$sfd_file}++;
       }
       last;
     }
@@ -100,12 +100,13 @@ sub parse_sfd_file($) {
     } elsif (/^Encoding:\s*\d+\s*(\d+)\s*\d+\s*$/) {
       $curenc = $1;
     } elsif ($curenc && !$empty && /^EndChar\s*/) {
-      inc_coverage ($curenc);
+      inc_coverage ($sfd_file, $curenc);
     }
   }
   close (F);
 }
 
+# TODO: formats would be better
 sub print_coverage() {
   print <<END;
 This is the Unicode coverage file for DejaVu fonts
@@ -114,37 +115,55 @@ This is the Unicode coverage file for DejaVu fonts
 Control and similar characters are discounted from totals.
 
 END
+  print "                                                ";
+  foreach $sfd_file (@sfd_files) {
+    my $label = $sfd_files{$sfd_file};
+    printf "%-19s", $label;
+  }
+  print "\n";
   foreach $block_start (sort { $a <=> $b } keys %blocks) {
     my ($block_end) = $blocks{$block_start}{'end'};
     my ($name) = $blocks{$block_start}{'name'};
-    my ($coverage) = $blocks{$block_start}{'coverage'};
-    $coverage = 0 if (!defined $coverage);
     my ($disabled) = scalar keys %{$blocks{$block_start}{'disabled'}};
     $disabled = 0 if (!defined $disabled);
     my ($length) = $block_end - $block_start + 1 - $disabled;
-    my ($percent) = $coverage/$length * 100;
-    printf "U+%04x %-40s ", $block_start, $name;
-    if ($percent > 0) {
-      printf "%3d%%", $percent;
-    } else {
-      print "    ";
+    printf "U+%04x %-40s", $block_start, $name;
+    foreach $sfd_file (@sfd_files) {
+      my ($coverage) = $blocks{$block_start}{'coverage'}{$sfd_file};
+      $coverage = 0 if (!defined $coverage);
+      my ($percent) = $coverage/$length * 100;
+      if ($percent > 0) {
+        printf " %3d%%", $percent;
+      } else {
+        print "     ";
+      }
+      printf " %-13s", "($coverage/$length)";
     }
-    print " ($coverage/$length)\n";
+    print "\n";
   }
 }
 
 if (@ARGV < 3) {
-  print STDERR "usage: unicode_data_file blocks_file sfd_file\n";
+  print STDERR "usage: unicode_data_file blocks_file sfd_file1 label1 (sfd_file2 label2...)\n";
   exit 1;
 }
 
 $unicode_data_file = shift @ARGV;
 $blocks_file = shift @ARGV;
-$sfd_file = shift @ARGV;
+@sfd_files = ();
+%sfd_files = ();
+while (@ARGV) {
+  $sfd_file = shift @ARGV;
+  $label = shift @ARGV;
+  push (@sfd_files, $sfd_file);
+  $sfd_files{$sfd_file} = $label;
+}
 
 parse_blocks_file($blocks_file);
 parse_unicode_data_file($unicode_data_file);
-parse_sfd_file($sfd_file);
+foreach $sfd_file (@sfd_files) {
+  parse_sfd_file($sfd_file);
+}
 print_coverage();
 
 if ($debug) {
