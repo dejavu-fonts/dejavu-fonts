dfe56fe51fa37672813f49cc60bd72f37f72ffa5
src <src@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2005-08-06 12:41:59 +0000

    added recalculation of glyphs count and positional encoding to sfdnormalize.pl

--- dejavu-fonts/sfdnormalize.pl
+++ dejavu-fonts/sfdnormalize.pl
@@ -3,7 +3,7 @@
 # $Id$
 
 # SFD normalizer (discards GUI information from SFD files)
-# (c)2004 Stepan Roh
+# (c)2004,2005 Stepan Roh
 # usage: ./sfdnormalize.pl sfd_file(s)
 #  will create files with suffix .norm
 
@@ -15,15 +15,26 @@
 #   Ref     - changed S (selected) to N (not selected)
 #   Fore, Back, SplineSet, Grid
 #           - all points have 4 masked out from flags (selected)
+#   recalculate number of characters and positional encoding
 
 # !!! Always review changes done by this utility !!!
 
-foreach $in (@ARGV) {
-  my $out = $in . '.norm';
-  open (IN, $in) || die "Unable to open $in : $!\n";
+sub process_sfd_file($);
+
+sub process_sfd_file($) {
+  my ($sfd_file) = @_;
+
+  my $out = $sfd_file . '.norm';
+  
+  open (SFD, $sfd_file) || die "Unable to open $sfd_file : $!\n";
   open (OUT, '>'.$out) || die "Unable to open $out : $!\n";
+
+  my $curchar = '';
+  my $glyphs = ();
   my $in_spline_set = 0;
-  while (<IN>) {
+  my $max_dec_enc = 0;
+
+  while (<SFD>) {
     next if (/^(WinInfo|DisplaySize):/);
     s,^(Flags:.*?)O(.*)$,$1$2,;
     s,^(Ref:.*?)S(.*)$,$1N$2,;
@@ -34,10 +45,63 @@ foreach $in (@ARGV) {
     } elsif ($in_spline_set) {
       s/(\s+)(\S+?)(,\S+\s*)$/$1.($2 & ~4).$3/e;
     }
-    print OUT;
+    if (/^BeginChars:/) {
+      $in_chars = 1;
+    } elsif (/^EndChars\s*$/) {
+      $in_chars = 0;
+      # adding of 1 to max_dec_enc is strange, but works
+      print OUT "BeginChars: ", $max_dec_enc + 1, " ", scalar (keys %glyphs), "\n";
+      foreach $glyph (sort { $glyphs{$a}{'dec_enc'} <=> $glyphs{$b}{'dec_enc'} } keys %glyphs) {
+        print OUT "StartChar: ", $glyphs{$glyph}{'name'}, "\n";
+        my $dec_enc = $glyphs{$glyph}{'dec_enc'};
+        my $mapped_enc = $glyphs{$glyph}{'mapped_enc'};
+        print OUT "Encoding: ", $dec_enc, " ", $mapped_enc, " ", $dec_enc, "\n";
+        print OUT @{$glyphs{$glyph}{'lines'}};
+        print OUT "EndChar\n";
+        $pos++;
+      }
+      print OUT "EndChars\n";
+    } elsif (/^StartChar:\s*(\S+)\s*$/) {
+      my $name = $1;
+      $curchar = $name;
+      while (exists $glyphs{$curchar}) {
+        $curchar .= '#';
+      }
+      $glyphs{$curchar}{'name'} = $name;
+    } elsif (/^Encoding:\s*(\d+)\s*((?:-|\d)+)\s*(\d+)\s*$/) {
+      $dec_enc = $1;
+      $max_dec_enc = $dec_enc if ($dec_enc > $max_dec_enc);
+      $mapped_enc = $2;
+      $pos = $3;
+      $glyphs{$curchar}{'dec_enc'} = $dec_enc;
+      $glyphs{$curchar}{'mapped_enc'} = $mapped_enc;
+      $glyphs{$curchar}{'pos'} = $pos;
+    } elsif (/^EndChar\s*$/) {
+      $curchar = '';
+    } else {
+      if (!$in_chars) {
+        print OUT;
+      } elsif ($curchar eq '') {
+        warn "Malformed input file $sfd_file?";
+      } else {
+        push (@{$glyphs{$curchar}{'lines'}}, $_);
+      }
+    }
   }
-  close (IN) || die "Unable to close $in : $!\n";
-  close (OUT) || die "Unable to close $out : $!\n";
+
+  close (SFD);
+  close (OUT);
+}
+
+if (!@ARGV) {
+  print STDERR "usage: sfd_files+\n";
+  exit 1;
+}
+
+@sfd_files = @ARGV;
+
+foreach $sfd_file (@sfd_files) {
+  process_sfd_file ($sfd_file);
 }
 
 1;
