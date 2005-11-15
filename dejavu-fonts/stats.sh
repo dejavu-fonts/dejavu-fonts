cc7c4b9c0f57281510983e983d42cb88536bce7d
src <src@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2005-11-15 21:21:02 +0000

    added simple statistics generator stats.sh

---
+++ dejavu-fonts/stats.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# $Id$
+
+# DejaVu fonts statistics generator
+# (c)2005 Stepan Roh
+# usage: ./stats.sh
+
+# Motto: "To proof that each task can be coded in very complicated way."
+
+echo "Version   New glyphs*)"
+echo "--------  ----------"
+versions=`grep 'U+' status.txt \
+ | tr -s ' ' \
+ | cut -d' ' -f 3 \
+ | sed 's,original,0.0,' \
+ | sed 's,^\(.\)\.\(.\)$,\1.0\2,' \
+ | sort \
+ | uniq \
+ | sed 's,^\(.\).0\(.\)$,\1.\2,' \
+ | sed 's,0\.0,original,'`
+for ver in $versions; do
+  count=`grep "^U+.*$ver\( \|$\)" status.txt | wc -l`
+  printf '%-8s %10i\n' $ver $count
+done
+echo
+echo "*) some glyphs may be counted multiple times if they were added to different faces in different versions"
