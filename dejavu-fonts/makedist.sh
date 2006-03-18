fdefe1dbdb052007909ebc95db724c07649bc87b
src <src@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2006-03-18 14:05:30 +0000

    pack langcover.txt as well

--- dejavu-fonts/makedist.sh
+++ dejavu-fonts/makedist.sh
@@ -6,9 +6,9 @@ version=$1
 echo Making distribution of DejaVu fonts $version
 mkdir packaged
 mkdir packaged/dejavu-sfd-$version
-cp *.sfd *.pe *.sh *.pl README LICENSE AUTHORS NEWS BUGS mes*.txt status.txt unicover.txt packaged/dejavu-sfd-$version
+cp *.sfd *.pe *.sh *.pl README LICENSE AUTHORS NEWS BUGS mes*.txt status.txt unicover.txt langcover.txt packaged/dejavu-sfd-$version
 (cd packaged; tar czvf dejavu-sfd-$version.tar.gz dejavu-sfd-$version)
 mkdir packaged/dejavu-ttf-$version
-cp generated/*.ttf README LICENSE AUTHORS NEWS BUGS status.txt unicover.txt packaged/dejavu-ttf-$version
+cp generated/*.ttf README LICENSE AUTHORS NEWS BUGS status.txt unicover.txt langcover.txt packaged/dejavu-ttf-$version
 (cd packaged; tar czvf dejavu-ttf-$version.tar.gz dejavu-ttf-$version)
 (cd packaged; zip -rv dejavu-ttf-$version.zip dejavu-ttf-$version)
