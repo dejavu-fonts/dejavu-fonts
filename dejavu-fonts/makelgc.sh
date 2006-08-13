69095d206a9bb650bf36a38eb4dcbf9c671465ec
ben_laenen <ben_laenen@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2006-08-13 15:57:31 +0000

    - add Latin Extended C/D and Modifier Tone Letters to the LGC release, next time we make a release (sorry, found this out too late for the 2.9 release)

--- dejavu-fonts/makelgc.sh
+++ dejavu-fonts/makelgc.sh
@@ -17,8 +17,11 @@ fontforge -script - *.sfd <<END
 i = 1
 while ( i < \$argc )
   Open(\$argv[i], 1)
+
   Select(0u0530, 0u1cff)
-  SelectMore(0u2c00, 0udfff)
+  SelectMore(0u2c00, 0u2c5f)
+  SelectMore(0u2cff, 0ua6ff)
+  SelectMore(0ua800, 0udfff)
   SelectMore(0uf900, 0ufaff)
   SelectMore(0ufb07, 0ufe1f)
   SelectMore(0ufe30, 0uffef)
