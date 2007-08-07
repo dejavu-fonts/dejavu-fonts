ba64614438056ee5d95c1be6aaf90ddb38a7b2a6
ben_laenen <ben_laenen@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2007-08-07 15:33:16 +0000

    * make the makelgc script work again

--- dejavu-fonts/makelgc.sh
+++ dejavu-fonts/makelgc.sh
@@ -26,25 +26,7 @@ for src in *.sfd; do
 done
 cd lgc
 echo "Stripping unwanted glyphs"
-fontforge -script - *.sfd <<END
-i = 1
-while ( i < \$argc )
-  Open(\$argv[i], 1)
-
-  Select(0u0530, 0u1cff)
-  SelectMore(0u2c00, 0u2c5f)
-  SelectMore(0u2cff, 0ua6ff)
-  SelectMore(0ua800, 0udfff)
-  SelectMore(0uf900, 0ufaff)
-  SelectMore(0ufb07, 0ufe1f)
-  SelectMore(0ufe30, 0uffef)
-#  SelectMore(0u10000, 0ueffff)
-  Clear()
-
-  Save(\$argv[i])
-  i++
-endloop
-END
+fontforge -script ../lgc.pe *.sfd
 echo "Generating TTF"
 mkdir generated
 ../generate.pe *.sfd
