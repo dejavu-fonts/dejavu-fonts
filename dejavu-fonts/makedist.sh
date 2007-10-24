4adfa6027cca68e6a24ef3258692129755abe7e9
ben_laenen <ben_laenen@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2007-10-24 18:11:52 +0000

    * makedist.sh needs to include Makefile as well

--- dejavu-fonts/makedist.sh
+++ dejavu-fonts/makedist.sh
@@ -6,7 +6,7 @@ version=$1
 echo Making distribution of DejaVu fonts $version
 mkdir packaged
 mkdir packaged/dejavu-sfd-$version
-cp *.sfd *.pe *.sh *.pl README LICENSE AUTHORS NEWS BUGS mes*.txt status.txt unicover.txt langcover.txt packaged/dejavu-sfd-$version
+cp *.sfd *.pe *.sh *.pl README LICENSE AUTHORS NEWS BUGS mes*.txt status.txt unicover.txt langcover.txt Makefile packaged/dejavu-sfd-$version
 (cd packaged; tar cjvf dejavu-sfd-$version.tar.bz2 dejavu-sfd-$version)
 mkdir packaged/dejavu-ttf-$version
 cp generated/*.ttf README LICENSE AUTHORS NEWS BUGS status.txt unicover.txt langcover.txt packaged/dejavu-ttf-$version
